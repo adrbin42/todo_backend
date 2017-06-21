2e2977c6f2cbc7984e44ef5efe6e790feee07789
adrienne <adriennebing@gmail.com>
2017-06-21 16:17:00 -0400

    added list functionality

--- app.js
+++ app.js
@@ -15,9 +15,38 @@ app.set('views','./views');
 app.set('view engine', 'mustache');
 
 app.get('/',function(req,res){
-  res.render('index',{name:'Adrienne Bing'});
+  res.render('index');
+});
+
+let todoItems = [];
+
+let i;
+let listItem = "";
+
+app.post('/', function(req,res){
+  todoItems.push(req.body.todoListItem);
+  res.redirect('/todo');
+});
+
+app.get('/todo', function(req,res){
+  res.render('completelist', {todoItems: todoItems});
 });
 
 app.listen(3000, function(){
   console.log('Running on localhost:3000');
 });
+
+// var newToDo = document.querySelector('.new-todo');
+// var todo_list = document.querySelector('.todo-list');
+//
+// newToDo.addEventListener('keypress', addToDo);
+//
+// function addToDo(event){
+//   if(event.keyCode == 13){
+//     var elemTarget = event.target;
+//     var listContent = elemTarget.value;
+//     elemTarget.value = "";
+//
+//     var textContent = '<li><div class="view"><label>' + listContent + '</label></div></li>';
+//     var newToDoItem = todo_list.insertAdjacentHTML('beforeend', textContent);
+//
